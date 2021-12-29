071a384855bf52c197fdd7e94f3c0f90538c3f06
Marpace <almonacid.pablo@hotmail.com>
2021-12-28 22:46:30 -0500

    started gallery

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+const portraitImages = [...document.querySelectorAll(".portrait")];
+const xsImages = [...document.querySelectorAll(".image-xs")];
+
+
+function populateImg(array, path){
+    let n = 1;
+    array.forEach(item => {
+        item.style.backgroundImage = `url("../../../images/gallery/gallery-img-${path} (${n}).jpg")`
+        n += 1;
+    });
+}
+
+populateImg(portraitImages, "portrait");
+populateImg(xsImages, "xs")
